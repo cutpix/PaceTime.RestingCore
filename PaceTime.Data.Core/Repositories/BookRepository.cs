81f76c8d3149586de2b25b63eb339b42d49354fb
Andrei Pavlyuk <devdice@outlook.com>
2018-02-04 04:59:17 +0200

    Implement GetBooks() method for books repos.

--- PaceTime.Data.Core/Repositories/BookRepository.cs
+++ PaceTime.Data.Core/Repositories/BookRepository.cs
@@ -1,16 +1,24 @@
 ﻿using PaceTime.Domain.Interfaces;
 using PaceTime.Domain.Models;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace PaceTime.Data.Core.Repositories
 {
     public sealed class BookRepository : IBookRepository
     {
+        private KnowledgeContext _context;
+
+        public BookRepository(KnowledgeContext context)
+        {
+            this._context = context;
+        }
+
         public IEnumerable<Book> GetBooks()
         {
-            throw new NotImplementedException();
+            return this._context.Books
+                                .OrderBy(b => b.Title)
+                                .ThenBy(b => b.Author.FirstName);
         }
     }
 }

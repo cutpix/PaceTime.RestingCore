e64c38e3677fd8fad0b6b715520286b9a3a647a9
Andrei Pavlyuk <devdice@outlook.com>
2018-02-11 11:33:32 +0200

    Added Security context for authentication

---
+++ PaceTime.Data.Security/SecurityDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PaceTime.Data.Security
+{
+    public class SecurityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public SecurityDbContext(DbContextOptions<SecurityDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+
+        }
+    }
+}

a8e3cb1b84b84a1d4250bad2e68a0dfa13364909
Andrei Pavlyuk <devdice@outlook.com>
2018-02-11 22:50:55 +0200

    Configure mvc services to throw 406 status, api will return responses in default supported formats if non supported media type is requested.

--- PaceTime.API/Startup.cs
+++ PaceTime.API/Startup.cs
@@ -43,7 +43,11 @@ namespace PaceTime.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddRazorPagesOptions(opt =>
+            services.AddMvc(opt =>
+            {
+                opt.ReturnHttpNotAcceptable = true;
+            })
+            .AddRazorPagesOptions(opt =>
             {
                 opt.RootDirectory = "/Pages";
             });

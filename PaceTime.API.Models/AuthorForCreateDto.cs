f85df55b43f822426b6ed4b2ed382aaa93654632
Andrei Pavlyuk <devdice@outlook.com>
2018-02-13 00:14:43 +0200

    adde children at once

--- PaceTime.API.Models/AuthorForCreateDto.cs
+++ PaceTime.API.Models/AuthorForCreateDto.cs
@@ -10,5 +10,8 @@ namespace PaceTime.API.Models
         public string LastName { get; set; }
         public DateTimeOffset DateOfBirth { get; set; }
         public string Genre { get; set; }
+
+        public ICollection<BookForCreationDto> Books { get; set; }
+        = new List<BookForCreationDto>();
     }
 }
